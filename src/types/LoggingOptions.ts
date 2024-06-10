ec13d170431151dac7091602adf196cf743d8232
Hovhannes Gasparyan <hovhannespost@gmail.com>
2024-06-10 13:20:53 +0400

    fix project dependancy

--- src/types/LoggingOptions.ts
+++ src/types/LoggingOptions.ts
@@ -1,8 +1,6 @@
-import {LogLevel} from "snowflake-sdk";
-
 export interface LoggingOptions {
   /** optional function to log SQL statements (e.g. console.log) */
   logSql?: (sqlText: string) => void;
   /** turn on SDK-level logging */
-  logLevel?: LogLevel;
+  logLevel?: any;
 }
