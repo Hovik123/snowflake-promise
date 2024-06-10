5e5fe76f39c21887b5da4cee59355a35efd1fa94
Hovhannes Gasparyan <hovhannespost@gmail.com>
2024-06-10 12:19:09 +0400

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Snowflake } from './Snowflake';
 export { Statement } from './Statement';
+export {ConnectionOptions} from 'snowflake-sdk';
 export { ExecuteOptions } from './types/ExecuteOptions';
 export { SnowflakeError } from './types/SnowflakeError';
 export { StatementAlreadyExecutedError } from './types/StatementAlreadyExecutedError';
